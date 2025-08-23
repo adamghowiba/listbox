3e8a914c99f9fcaf2b97a5e47dfb6620a4c9b3f2
Adam Ghowiba <adamware99@hotmail.com>
2025-08-24 03:04:02 +0400

    feat: add gluestack button

--- apps/listbox-mobile/app/(tabs)/index.tsx
+++ apps/listbox-mobile/app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
+import { Button, ButtonIcon, ButtonText } from '@/libs/components/ui/button';
 import { SafeAreaView, Text } from 'react-native';
-
 
 export default function HomeScreen() {
   return (
-   <SafeAreaView>
-    <Text style={{color: 'red'}}>Hello</Text>
-   </SafeAreaView>
+    <SafeAreaView>
+      <Text style={{ color: 'red' }}>Hello</Text>
+
+      <Button variant="solid" action="secondary">
+        <ButtonText>Hello</ButtonText>
+      </Button>
+    </SafeAreaView>
   );
 }
-
